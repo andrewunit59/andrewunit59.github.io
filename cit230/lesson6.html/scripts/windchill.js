18ff11630b4b0509a343b2826acdbdcb02a0a3fe
Andrew <mar17035@byui.edu>
2019-10-25 16:02:55 -0600

    Hero Image attempt

---
+++ cit230/lesson6.html/scripts/windchill.js
@@ -0,0 +1,12 @@
+let currentTemp = 41;
+let windSpeed = 8;
+let windChill = 0;
+
+if  (currentTemp < 50 && windSpeed > 3) {
+ windChill =35.74 + 0.6215 * currentTemp - 35.75 * Math.pow(windSpeed, 0.16) 
+    + 0.4275 * currentTemp * Math.pow(windSpeed, 0.16);
+}
+
+document.getElementById("currentTemp").innerHTML = currentTemp + '&deg;';
+document.getElementById("windSpeed").innerHTML = windSpeed + ' mph';
+document.getElementById("windChill").innerHTML = Math.round(windChill) + '&deg;'; 
