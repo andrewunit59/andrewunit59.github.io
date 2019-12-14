257dfd36b4363baf8b8d82660090abee0980a99b
Andrew <mar17035@byui.edu>
2019-12-14 16:11:38 -0700

    Final Project Submit

--- cit230/FinalProject/scripts/temples.js
+++ cit230/FinalProject/scripts/temples.js
@@ -1,11 +1,24 @@
 fetch('json/temples.json')
     .then(
-        (response) => response.json()
+        (response) => response.json() 
     )
     .then(
-        (temples).forEach(
-            (temple) => {
-                console.log(temple.TempleName);
+            (res) => {
+                let templeDiv = "";
+                for (let temple of res.temples) {
+                    
+                    for (let i = 0; i < temple.length; i++ ) {
+                        let card = document.createElement('section');
+                        let h2 = document.createElement('h2');
+
+                        h2.textContent = temples[i].TempleName;
+                        card.appendChild(h2);
+
+                        document.querySelector('div#my-cards').appendChild(card);
+                    }
+                    
+                    
+                }
+                document.getElementById('my-cards').innerHTML = templeDiv;
             }
-        )
-    )
+        );
