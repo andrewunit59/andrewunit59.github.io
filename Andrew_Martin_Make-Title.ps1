6df05730e1e5eaa03e66d940a4885b464b2a060c
Andrew <mar17035@byui.edu>
2020-03-18 19:39:13 -0600

    WebMountPrep

--- Andrew_Martin_Make-Title.ps1
+++ Andrew_Martin_Make-Title.ps1
@@ -15,11 +15,11 @@ else{
     foreach($word in $words){
 
         if($firstword){
-            proper $word
+            $fixed += proper $word
             $firstword=$false
         }else{
-            if($word -in $prepositions){
-                $word.ToLower()
+            if($word -in $propositions){
+                $fixed+=$word.ToLower()
             }else{
             $fixed += proper $word
         }
@@ -27,32 +27,3 @@ else{
     }
     $fixed -join ' '
 }
-
-# #Propositions are still capitialized, and they shouldn't be.
-# param([string] $title, [switch] $AllCaps, [string] $propositions)
-
-# function MakeTitle ([string] $title, [switch] $AllCaps) {
-#     if($AllCaps){
-#         $title.ToUpper()
-#      } else{
-#          proper $title
-#      }}
-
-
-#  function proper ([string] $title){
-#          $propositions=('to', 'a','the', 'at', 'in', 'of', 'with', 'and', 'but', 'or', 'is')
-#          [string]$propositions
-#          $propositions.GetType()
-#          ($title -split ' ' | ForEach-Object{
-#              if($_ -notin $propositions){
-#                  "$([char]::ToUpper($_[0]))$($_.substring(1).toLower())"
-#              }elseif($_[0] -in $propositions){
-#                  "$([char]::ToUpper($_[0]))$($_.substring(1).toLower())"
-#              }else{
-#                  "$([char]::ToUpper($_[0]))$($_.substring(1).toLower())"
-#              } 
-#          }) -join ' '
-#      }
-
-#      MakeTitle $title -AllCaps:$AllCaps
-
